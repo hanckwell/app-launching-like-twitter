f1d6183ec701aa6cef24d5902f12a669a50c44dd
Александр Клёмин <hanckwell@gmail.com>
2020-05-05 23:53:54 +0300

    App_Launching_like_TwitterTests conversion to Swift 4 applied.

--- App Launching like TwitterTests/App_Launching_like_TwitterTests.swift
+++ App Launching like TwitterTests/App_Launching_like_TwitterTests.swift
@@ -28,7 +28,7 @@ class App_Launching_like_TwitterTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
